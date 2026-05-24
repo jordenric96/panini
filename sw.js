1b6dab75118ec7092b8fe508fc565db9ef0aecea
jordenric96 <jordenricour96@gmail.com>
2026-05-24 15:28:31 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,24 @@
-// sw.js - Service Worker v9
-const CACHE_NAME = 'panini-tracker-v9';
+// sw.js - Service Worker v12
+const CACHE_NAME = 'panini-tracker-v12';
 const ASSETS = [
   './index.html',
-  './style.css?v=9',
-  './countries.js?v=9',
-  './app.js?v=9',
+  './style.css?v=12',
+  './countries.js?v=12',
+  './app.js?v=12',
   './manifest.json'
 ];
 
 self.addEventListener('install', (e) => {
   self.skipWaiting(); 
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME) {
-          return caches.delete(key); 
-        }
-      }));
-    })
-  );
+  e.waitUntil(caches.keys().then((keyList) => {
+    return Promise.all(keyList.map((key) => { if (key !== CACHE_NAME) return caches.delete(key); }));
+  }));
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => {
-      return caches.match(e.request);
-    })
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
