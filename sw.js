2c92c666cff4fdf42299b175c846bcee5cedfd04
jordenric96 <jordenricour96@gmail.com>
2026-05-28 19:48:37 +0200

    Update sw.js``

--- sw.js
+++ sw.js
@@ -1,12 +1,11 @@
 // sw.js - Service Worker v14
-const CACHE_NAME = 'panini-tracker-v14';
-const ASSETS = [
-  './index.html',
-  './style.css?v=14',
-  './countries.js?v=14',
-  './app.js?v=14',
-  './manifest.json'
-];
+// sw.js - Service Worker v15
+const CACHE_NAME = 'panini-tracker-v15';
+const ASSETS = [ './index.html', './style.css?v=15', './countries.js?v=15', './app.js?v=15', './manifest.json' ];
+
+self.addEventListener('install', (e) => { self.skipWaiting(); e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))); });
+self.addEventListener('activate', (e) => { e.waitUntil(caches.keys().then((keyList) => { return Promise.all(keyList.map((key) => { if (key !== CACHE_NAME) return caches.delete(key); })); })); });
+self.addEventListener('fetch', (e) => { e.respondWith(fetch(e.request).catch(() => caches.match(e.request))); });
 
 self.addEventListener('install', (e) => {
   self.skipWaiting(); 
