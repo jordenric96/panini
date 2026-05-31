ad4bc219b10301b5011d6933e972a25e211f7697
jordenric96 <jordenricour96@gmail.com>
2026-05-31 20:29:26 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,9 @@
 // sw.js - Service Worker v14
 // sw.js - Service Worker v15
 // sw.js - Service Worker v16
-const CACHE_NAME = 'panini-tracker-v16';
-const ASSETS = [ './index.html', './style.css?v=16', './countries.js?v=16', './app.js?v=16', './manifest.json' ];
+const CACHE_NAME = 'panini-tracker-v19';
+const ASSETS = [ './index.html', './style.css?v=19', './countries.js?v=19', './app.js?v=19', './manifest.json' ];
+// ...
 
 self.addEventListener('install', (e) => { self.skipWaiting(); e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))); });
 self.addEventListener('activate', (e) => { e.waitUntil(caches.keys().then((keyList) => { return Promise.all(keyList.map((key) => { if (key !== CACHE_NAME) return caches.delete(key); })); })); });
